f16d0bd1d7a071af27355db9ebe3951628c388a5
DeNeMiX83 <daniil53891@yandex.ru>
2022-04-28 15:39:03 +0300

    fix

---
+++ src/com/company/Figure.java
@@ -0,0 +1,7 @@
+package com.company;
+
+interface Figure{
+    public double perimeter();
+    public double area();
+    public Figure add(R2Point p);
+}
---
+++ src/com/company/Void.java
@@ -0,0 +1,16 @@
+package com.company;
+
+//Класс "Нульугольник", реализующий интерфейс фигуры.
+class Void implements Figure{
+    public double perimeter(){
+        return 0.0;
+    }
+
+    public double area(){
+        return 0.0;
+    }
+
+    public Figure add(R2Point p){
+        return new Point(p);
+    }
+}
