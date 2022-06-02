16886f17364d3847d3c4c29dc8ff9b5c99b70f85
mihey <daniil53891@yandex.ru>
2022-06-02 12:52:36 +0300

    Task 10.4

--- src/com/company/Task10_4/Convex.java
+++ src/com/company/Task10_4/Convex.java
@@ -24,8 +24,6 @@ class Convex{
 
     public double getlengthFromStraight(int x1, int y1, int x2, int y2, ConvexFrame frame){
         double length = Integer.MAX_VALUE;
-        double old_length = length;
-        R2Point[] ps = new R2Point[2];
 
         ArrayList<R2Point> points = this.getAllPoints();
         for (int i = 0; i < points.size(); i++) {
@@ -37,25 +35,17 @@ class Convex{
             R2Point point1 = points.get(i);
             R2Point point2 = points.get(j);
 
-            old_length = length;
             length = Math.min(
                     Math.min(lengthFromStraight(x1, y1, x2, y2, point1),
                              lengthFromStraight(x1, y1, x2, y2, point2)),
                     length
             );
-            if (old_length != length){
-                ps[0] = point1;
-                ps[1] = point2;
-            }
         }
         return length;
     }
 
     public double lengthFromStraight(int x1, int y1, int x2, int y2, R2Point p) {
         double numerator = (y1 - y2) * p.getX() + (x2 - x1) * p.getY() + (x1*y2 - x2*y1);
-//        if (numerator == 0.0){
-//            return Integer.MAX_VALUE;
-//        }
         double denominator = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
         return Math.abs(numerator / denominator);
     }
